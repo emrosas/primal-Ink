936b432a63739ddaac24ecf8f63f876ee6a3a6d4
Erick Mireles <emireles.rosas@gmail.com>
2022-07-05 12:41:58 -0500

    Added the "About" and "Services" sections.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./**/*{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark': '#262626',
+        'light': '#FFFCF7',
+        'shell': '#FFF5EC',
+        'brand': '#FF0054',
+      }
+    },
     fontFamily: {
       'thunder': ["'Thunder'", 'sans-serif'],
       'display': ["'NT Wagner'", 'sans-serif'],
