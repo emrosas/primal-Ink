20b494f07f39fd8c6fe64ac19d6a003c47eaee0f
Erick Mireles <emireles.rosas@gmail.com>
2022-07-04 22:50:36 -0500

    Added tailwindcss & custom fonts

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*{html, js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'thunder': ["'Thunder'", 'sans-serif'],
+      'display': ["'NT Wagner'", 'sans-serif'],
+      'body': ["'Poppins'", 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
